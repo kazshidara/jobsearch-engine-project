88bbbfebf63fd19c046d6107b0dc40afcdb007c0
Kazune Shidara <kazshidara@ucdavis.edu>
2019-09-19 23:02:21 +0000

    Version up for requirements.txt

--- server.py
+++ server.py
@@ -521,7 +521,7 @@ def show_events():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -529,6 +529,6 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # Use the DebugToolbar
-    DebugToolbarExtension(app)
+    # DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
